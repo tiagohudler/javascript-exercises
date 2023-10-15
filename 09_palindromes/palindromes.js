d2fcb683e62f8a4828257a2f8321070d3caa816f
Tiago Hudler <tiagohudler@gmail.com>
2023-10-15 13:19:32 -0300

    solved to exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let veter = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    if(veter == veter.split("").reverse().join("")) return true;
+    else return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    if (arg < 0) return "OOPS"
+    let atual = 1;
+    let anterior = 0;
+    let proximo;
+    while (arg > 1){
+        proximo = atual + anterior;
+        anterior = atual;
+        atual = proximo;
+        arg--;
+    }
+    return atual;
 };
 
 // Do not edit below this line
