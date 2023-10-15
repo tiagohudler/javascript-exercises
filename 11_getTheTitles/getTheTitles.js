749f56ec54920abd443d74ec95f164b1b8495deb
Tiago Hudler <tiagohudler@gmail.com>
2023-10-15 13:42:50 -0300

    solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+    let len = array.length;
+    const books = [];
+    for (let i = 0; i < len; i++){
+        books.push(array[i].title);
+    }
+    return books;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
