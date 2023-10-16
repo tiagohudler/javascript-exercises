794158a6e31c1efa0c33310a25c6725b671a22af
Tiago Hudler <tiagohudler@gmail.com>
2023-10-16 17:29:00 -0300

    exercise 12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldest;
+    let highestAge = 0;
+    let len = array.length;
+    for (let i = 0; i < len; i++){
+        let currentAge;
+        if (array[i].yearOfDeath) {currentAge = array[i].yearOfDeath - array[i].yearOfBirth;}
+        else {currentAge = 2023 - array[i].yearOfBirth;}
+        if (currentAge > highestAge){
+            highestAge = currentAge;
+            oldest = i;
+        }
+    }
+    return array[oldest];
 };
 
 // Do not edit below this line
